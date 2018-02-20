529ecda4e7e8c4415e9368be44f3b42ce381cd11
Celie Baker <celie.n133@gmail.com>
2018-02-20 10:02:30 -0700

    In class JS practice

---
+++ practice/action.js
@@ -0,0 +1,8 @@
+let color = "red";
+let animal = "cat";
+
+console.log(color, animal);
+
+document.getElementById("container").style.backgroundColor = "#845";
+document.getElementById("container").style.color = "#236";
+document.getElementById("container").style.fontSize = "50px";
