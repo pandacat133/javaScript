b5536fdc5793d1c8674c188183932f3978768c7a
Celie Baker <celie.n133@gmail.com>
2018-02-20 10:12:56 -0700

    Added a dynamic for loop

--- practice/action.js
+++ practice/action.js
@@ -1,8 +1,15 @@
 let color = "red";
 let animal = "cat";
 
-console.log(color, animal);
+var myDiv = document.getElementById("container");
 
 document.getElementById("container").style.backgroundColor = "#845";
 document.getElementById("container").style.color = "#236";
 document.getElementById("container").style.fontSize = "50px";
+
+
+for(var i = 0; i < 5; i++){
+    myDiv.innerHTML += "<div>Hello</div>";
+}
+
+console.log(color, animal);
