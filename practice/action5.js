70cba0005772c2eac33e310b0eef09822209c727
Celie Baker <celie.n133@gmail.com>
2018-02-27 09:54:32 -0700

    addition calculator and iteration example

--- practice/action5.js
+++ practice/action5.js
@@ -1,3 +1,28 @@
-function celie(featureOne, featureTwo) {
-    console.log('Hello Celie, you are ' + featureOne + ' and ' + featureTwo + '.');
+function addition(){
+    let boxOne = document.getElementById('numberOne').value;
+    let boxTwo = document.getElementById('numberTwo').value;
+    var finalAnswer = runThis(boxOne, boxTwo);
+
+    document.getElementById('answer').innerHTML = finalAnswer;
 }
+
+function runThis(valueOne, valueTwo) {
+    return Number(valueOne) + Number(valueTwo);
+}
+
+function makeIterator(array) {
+    var nextIndex = 0;
+
+    return {
+        next: function() {
+            return nextIndex < array.length ?
+                {value: array[nextIndex++], done: false} :
+                {done: true};
+        }
+    }
+}
+
+var it = makeIterator(['yo', 'ya']);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().done);
