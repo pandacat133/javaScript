c0b42790a8d62beb90c07a63f123bae6197600ab
Celie Baker <celie.n133@gmail.com>
2018-03-01 08:32:32 -0700

    Messing around with objects

---
+++ practice/objects.js
@@ -0,0 +1,17 @@
+let chewie = {
+    name: 'chewie',
+    job: 'smugglers assistant',
+    hobby: 'space chess',
+    species: 'wookie',
+    language: 'rawr',
+    skills: 'crossbow',
+    actScore: '23'
+};
+
+document.getElementById('name').innerHTML = chewie.name;
+document.getElementById('job').innerHTML = chewie.job;
+document.getElementById('hobby').innerHTML = chewie.hobby;
+document.getElementById('species').innerHTML = chewie.species;
+document.getElementById('language').innerHTML = chewie.language;
+document.getElementById('skills').innerHTML = chewie.skills;
+document.getElementById('actScore').innerHTML = chewie.actScore;
