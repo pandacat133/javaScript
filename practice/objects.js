9314b7f0e2d74b1afc485dae29a536fc0bd8853d
Celie Baker <celie.n133@gmail.com>
2018-03-01 09:45:41 -0700

    Objects and reflections examples

--- practice/objects.js
+++ practice/objects.js
@@ -1,17 +1,54 @@
-let chewie = {
-    name: 'chewie',
-    job: 'smugglers assistant',
-    hobby: 'space chess',
-    species: 'wookie',
-    language: 'rawr',
-    skills: 'crossbow',
-    actScore: '23'
+//blueprint for a monster
+let Monster = function(mname) {
+
+    this.name = mname;
+    this.health = 80;
+
+    this.jump = function() {
+        console.log('monster jumped');
+    };
+    this.crouch = function () {
+        console.log('monster crouched')
+    };
+
 };
 
-document.getElementById('name').innerHTML = chewie.name;
-document.getElementById('job').innerHTML = chewie.job;
-document.getElementById('hobby').innerHTML = chewie.hobby;
-document.getElementById('species').innerHTML = chewie.species;
-document.getElementById('language').innerHTML = chewie.language;
-document.getElementById('skills').innerHTML = chewie.skills;
-document.getElementById('actScore').innerHTML = chewie.actScore;
+
+//creating an instance of the monster, named fred
+let fred = new Monster('Fred');
+fred.jump();
+
+
+//creating a different instance of the monster, named bob
+let bob = new Monster('Bob');
+bob.crouch();
+
+
+//reflection blueprint
+let Reflector = function(obj) {
+    this.getProperties = function(){
+        let properties = [];
+        for (let prop in obj) {
+            if (typeof obj[prop]!='function') {
+                properties.push(prop);
+            }
+        }
+        return properties;
+    };
+
+    this.getMethods = function(){
+        let methods = [];
+        for (let meth in obj) {
+            if (typeof obj[meth] === 'function') {
+                methods.push(meth);
+            }
+        }
+        return methods;
+    };
+};
+
+
+//reflection instance
+let mirror = new Reflector(fred);
+console.log(mirror.getProperties());
+console.log(mirror.getMethods());
