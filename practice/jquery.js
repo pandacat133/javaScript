81e124d3eed4e549bb988c4f8170926bf28a5fe4
Celie Baker <celie.n133@gmail.com>
2018-03-16 08:41:35 -0600

    not sure what this is but it's not important

--- practice/jquery.js
+++ practice/jquery.js
@@ -1,7 +1,8 @@
 function createAList() {
-    var listName = $('#createInput').val();
-    $('.createdLists').append('<div>'+ listName + '</div>');
-    $('#createInput').val('');
+var listName = $('#createInput').val();
+$('.createdLists').append('<div>'+ listName + '</div>');
+$('#createInput').val('');
 }
 
 //<div class="editbox" contenteditable=true></div>
+//use .localstorage to store to-do list locally
