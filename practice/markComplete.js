5a289aad89be4782388f130190ddf50831b6b2a9
Celie Baker <celie.n133@gmail.com>
2018-03-20 07:50:37 -0600

    To do list stuff

--- practice/markComplete.js
+++ practice/markComplete.js
@@ -1,7 +1,16 @@
-$(document).ready(function() {
+function markComplete(myElement) {
+    let myGrandpa = $(myElement).parent().parent();
+    $(myGrandpa).toggleClass('complete');
+    $(myGrandpa).find('.iconSpot').toggleClass('fa-check');
+}
 
-    $('.fa-square').on('click', function() {
-        $(this).toggleClass('fa-square fa-check-square');
+function removeRow(theElement) {
+    let myGrandma = $(theElement).parent().parent();
+
+    $(myGrandma).animate({
+        opacity: 0,
+        left: '+=50'
+    }, 400, function () {
+        $(myGrandma).remove();
     });
-
-});
+}
