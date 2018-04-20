39793dadaed04b56701099b3b3a7b1751e09d135
Celie Baker <celie.n133@gmail.com>
2018-04-20 09:10:56 -0600

    adding scores together (golf project) example

--- practice/domManipulation.js
+++ practice/domManipulation.js
@@ -1,4 +1,11 @@
-function myFunction(myLink) {
-    let myDemo = document.getElementById('toHere');
-    myDemo.innerHTML = myLink.innerHTML;
+let numberOfHoles = 6;
+
+function addScore(myValue) {
+    let tempScore = 0;
+    for(let i = 1; i <= numberOfHoles; i++) {
+        let inValue = Number($('#p' + myValue + 'h' + i).val());
+        tempScore += inValue;
+    }
+
+    $('.total' + myValue).html(tempScore);
 }
